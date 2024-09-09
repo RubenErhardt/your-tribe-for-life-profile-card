e0e124c208fb4834dec84b2ade9bc1564bb2914b
RubenErhardt <rubengamebattles@outlook.com>
2024-09-09 22:37:01 +0200

    sveltekit install + first build

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div class="container">
+    <button>Click to know me</button>
+</div>
+
+<style>
+ .container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh; 
+        }
+        
+        button {
+        padding: 15px 40px;
+        font-size: 20px;
+        font-weight: bold;
+        color: #ffffff;
+        background: linear-gradient(135deg, #000000, #242222);
+        border: none;
+        border-radius: 50px;
+        box-shadow: 0 10px 15px rgba(0, 0, 0, 0.4);
+        cursor: pointer;
+        }
+
+        
+</style>
